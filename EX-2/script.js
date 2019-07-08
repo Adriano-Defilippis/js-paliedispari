01e168b78e7f68dc6ae17d4ba0bf6ec0af85cd40
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-09 00:06:48 +0200

    iNSERITA LA FUNZIONE PER CONTROLLARE CHE L'UTENTE DIGITI O PARI O DISPARI

--- EX-2/script.js
+++ EX-2/script.js
@@ -2,13 +2,39 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer e
 // dichiariamo chi ha vinto.
 
-var scelta = prompt('Pari o dispari..? Tenta la fortuna');
 
-// Funzione per generare numeri randoms
+
+// Funzione per controllare se l'input corrispinde a pari o dispari
+function controlloInput(input) {
+  var input = input.toUpperCase();
+
+  console.log("Input iniziale: ", input);
+
+  // Controllo sul tipo di input inserito
+  if (input !== "DISPARI" || input !== "PARI"){
+
+    alert('Devi scegliere o pari o dispari amico bello');
+
+    input = prompt('Reinserisci la parola');
+
+    console.log("Siamo nel ciclo while se imput pari o dispari: ",input);
+
+  }
+
+}
+
+
+// Funzione per generare numeri random
 function randomNum(){
   var num = Math.floor(Math.random() * 21);
 
   console.log(num);
 }
+
+
+// Esecuzione del codice
+var scelta = prompt('Pari o dispari..? Tenta la fortuna');
+
+controlloInput(scelta);
 
 randomNum();
