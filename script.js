df66f4ee2f5f21877f6269e7481265a0fd55318b
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-08 23:25:16 +0200

    Due funzioni con parametro: _ La prima controlla il tipo di input; _La seconda fa un reverse della stringa e controlla se è unguale alla stringa di input

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-// Creo una funzione per capire se la parola è palindroma;
+// Funzione per controllare se l'input inserito è una stringa
+function controlloInput(input) {
+  var input;
 
-function parPalindroma(input) {
-  var input = prompt("Inserisci una parola palindroma");
+
+  var messaggio;
+
 
   console.log("Input iniziale: ", input);
 
   // Controllo sul tipo di input inserito
   while (!isNaN(input)){
 
-    console.log("Siamo nel ciclo while se l'input è un numero: ",input);
-
     alert('I numeri non sono palindromi');
 
     input = prompt("Inserisci una parola palindroma");
+
+    console.log("Siamo nel ciclo while se l'input è un numero: ",input);
+
   }
+
 
 }
 
-console.log(parPalindroma());
+
+// Creo una funzione per capire se la parola è palindroma;
+function parPalindroma(input){
+
+  var input;
+  var messaggio = "La parola inserita non risulta essere palindroma"
+  var reverseStr = '';
+
+  for(var i = input.length-1; i>= 0; i--){
+reverseStr += input[i];
+}
+
+console.log("Stringa reversata  ",reverseStr);
+
+  // Confronto tra stringa inserita e stringa reversata
+  if(reverseStr !== input){
+
+    messaggio;
+
+  }
+  else{
+    messaggio = 'La stringa è Palindroma!'
+  }
+  alert(messaggio);
+
+}
+
+var parola  = prompt("Inserisci una parola palindroma");
+
+controlloInput(parola);
+
+parPalindroma(parola);
